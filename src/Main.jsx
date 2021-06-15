1ca53015749754a5b191aa09d2ad02af03064cc3
Elena Pavlova <lenlho22@gmail.com>
2021-06-15 17:35:00 +0300

    Get fieldValue and remove checkIsFieldSetted

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { SiteClient } from 'datocms-client';
 import PropTypes from 'prop-types';
 
 import connectToDatoCms from './connectToDatoCms';
@@ -15,39 +14,11 @@ const getRandomKey = (length) => {
   return keyRand1;
 };
 
-const toCamel = s => s.replace(/([-_][a-z])/ig, $1 => $1.toUpperCase()
-  .replace('-', '')
-  .replace('_', ''));
-
 const Main = ({
-  plugin, datoCmsApiToken, idPrefix, minLength = 3, autoGeneration, isUpperCase,
+  plugin, idPrefix, minLength = 3, autoGeneration, isUpperCase,
 }) => {
-  const [loading, setLoading] = useState(false);
-  const [shouldAutoGenerate, setShouldAutoGenerate] = useState(false);
   const [adjustedMinLength] = useState(minLength < MIN_LENGTH ? MIN_LENGTH : minLength);
-
-  const checkIsFieldSetted = useCallback(() => {
-    setLoading(true);
-
-    const client = new SiteClient(datoCmsApiToken);
-
-    client.items.find(plugin.itemId)
-      .then((currentItem) => {
-        const convertedApiKey = toCamel(plugin.field.attributes.api_key);
-        const currentValue = currentItem[convertedApiKey];
-
-        if (!currentValue) {
-          setShouldAutoGenerate(true);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        plugin.notice('An error has occured, please try again.');
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, [plugin, datoCmsApiToken]);
+  const fieldValue = plugin.getFieldValue(plugin.fieldPath);
 
   const generateRandomId = useCallback(() => {
     const randomPart = isUpperCase
@@ -57,44 +28,25 @@ const Main = ({
     const randomId = idPrefix ? `${idPrefix}-${randomPart}` : randomPart;
 
     plugin.setFieldValue(plugin.fieldPath, randomId);
-    plugin.notice(`${plugin.field.attributes.label} generated successfully.`);
   }, [adjustedMinLength, idPrefix, isUpperCase, plugin]);
 
   useEffect(() => {
-    if (autoGeneration) {
-      if (plugin.itemId) {
-        checkIsFieldSetted();
-      } else {
-        setShouldAutoGenerate(true);
-      }
-    }
-  }, [autoGeneration, checkIsFieldSetted, plugin.itemId]);
-
-  useEffect(() => {
-    if (shouldAutoGenerate) {
+    if (autoGeneration && !fieldValue) {
       generateRandomId();
     }
-  }, [shouldAutoGenerate, generateRandomId]);
-
+  }, []);
 
   return (
-    <>
-      {loading ? (
-        <p className="loading">Loading...</p>
-      ) : (
-        <a href="#generate" className="link" onClick={generateRandomId}>
-          Generate
-          {' '}
-          {plugin.field.attributes.label.toLowerCase()}
-        </a>
-      )}
-    </>
+    <a href="#generate" className="link" onClick={generateRandomId}>
+      Generate
+      {' '}
+      {plugin.field.attributes.label.toLowerCase()}
+    </a>
   );
 };
 
 const Wrap = connectToDatoCms(plugin => ({
   plugin,
-  datoCmsApiToken: plugin.parameters.global.datoCmsApiToken,
   idPrefix: plugin.parameters.instance.idPrefix,
   minLength: plugin.parameters.instance.minLength,
   autoGeneration: plugin.parameters.instance.autoGeneration,
@@ -103,7 +55,6 @@ const Wrap = connectToDatoCms(plugin => ({
 
 Main.propTypes = {
   plugin: PropTypes.object.isRequired,
-  datoCmsApiToken: PropTypes.string.isRequired,
   idPrefix: PropTypes.string,
   minLength: PropTypes.number.isRequired,
   autoGeneration: PropTypes.bool.isRequired,
